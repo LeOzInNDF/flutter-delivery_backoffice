dc2662979d52a0a9173cdd69cf333ac6b16a52fa
LeOzInNDF <leo_albano@hotmail.com>
2023-05-10 23:56:12 -0300

    Initial commit

---
+++ lib/src/modules/home/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import '../../core/ui/helpers/loader.dart';
+import '../../core/ui/helpers/messages.dart';
+import '../../core/ui/helpers/size_extensions.dart';
+import '../../core/ui/styles/colors_app.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader, Messages {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          showLoader();
+          await Future.delayed(const Duration(seconds: 2));
+          hideLoader();
+
+          showError('Erro de alguma coisa');
+        },
+      ),
+      body: Container(
+        child: Container(
+          color: context.colors.primary,
+          width: context.percentWidth(.5),
+          height: context.percentHeight(.9),
+        ),
+      ),
+    );
+  }
+}
