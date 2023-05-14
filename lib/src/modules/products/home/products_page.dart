308323ffdf2d63453ac3fdb5830b8f66b4c0e1e9
LeOzInNDF <leo_albano@hotmail.com>
2023-05-13 21:19:24 -0300

    implements Page detail

--- lib/src/modules/products/home/products_page.dart
+++ lib/src/modules/products/home/products_page.dart
@@ -1,10 +1,57 @@
-import 'package:delivevy_backoffice/src/modules/products/home/widgets/product_item.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx/mobx.dart';
+
+import '../../../core/ui/helpers/debouncer.dart';
+import '../../../core/ui/helpers/loader.dart';
+import '../../../core/ui/helpers/messages.dart';
+import 'widgets/product_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 import '../../../core/ui/widgets/base_header.dart';
+import 'products_controller.dart';
 
-class ProductsPage extends StatelessWidget {
+class ProductsPage extends StatefulWidget {
   const ProductsPage({super.key});
+
+  @override
+  State<ProductsPage> createState() => _ProductsPageState();
+}
+
+class _ProductsPageState extends State<ProductsPage> with Loader, Messages {
+  final controller = Modular.get<ProductsController>();
+  late final ReactionDisposer statusDisposer;
+  final debouncer = Debouncer(milliseconds: 500);
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      statusDisposer = reaction((_) => controller.status, (status) {
+        switch (status) {
+          case ProductStateStatus.initial:
+            break;
+          case ProductStateStatus.loading:
+            showLoader();
+            break;
+          case ProductStateStatus.loaded:
+            hideLoader();
+            break;
+          case ProductStateStatus.error:
+            hideLoader();
+            showError('Erro ao buscar produtos');
+            break;
+        }
+      });
+      controller.loadProducts();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    statusDisposer();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,21 +64,30 @@ class ProductsPage extends StatelessWidget {
             title: 'ADMINISTRAR PRODUTOS',
             buttonLabel: 'ADICIONAR PRODUTO',
             buttonPressed: () {},
+            searchChange: (value) {
+              debouncer.call(() {
+                controller.filterByName(value);
+              });
+            },
           ),
           const SizedBox(
             height: 50,
           ),
           Expanded(
-            child: GridView.builder(
-              itemCount: 10,
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                mainAxisExtent: 280,
-                mainAxisSpacing: 20,
-                maxCrossAxisExtent: 280,
-                crossAxisSpacing: 10,
-              ),
-              itemBuilder: (context, index) {
-                return const ProductItem();
+            child: Observer(
+              builder: (_) {
+                return GridView.builder(
+                  itemCount: controller.products.length,
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    mainAxisExtent: 280,
+                    mainAxisSpacing: 20,
+                    maxCrossAxisExtent: 280,
+                    crossAxisSpacing: 10,
+                  ),
+                  itemBuilder: (context, index) {
+                    return ProductItem(product: controller.products[index]);
+                  },
+                );
               },
             ),
           )
